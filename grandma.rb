76b31a54d9459a5bb867c448ace43c334c4f1439
Doug Fitzgerald <dfitzge1@conncoll.edu>
2018-09-12 16:40:36 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -2,10 +2,10 @@
 def speak_to_grandma(phrase)
   if phrase == "I LOVE YOU GRANDMA!"
     puts "I LOVE YOU TOO PUMPKIN!"
-  end 
+  
   elsif phrase.upcase == phrase
     puts "NO, NOT SINCE 1938!"
-  end 
+   
   else
     puts "HUH?! SPEAK UP, SONNY!"
   end 
